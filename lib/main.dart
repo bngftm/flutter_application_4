c3c722ec9d91001881762b4dec39176c5fa5c88d
bngftm <2499610321@qq.com>
2022-06-13 01:18:04 +0800

    新增

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,30 +26,18 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ThemeTestRoute(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ThemeTestRoute extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ThemeTestRouteState createState() => _ThemeTestRouteState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _ThemeTestRouteState extends State<ThemeTestRoute> {
+  var _themeColor = Colors.yellow; //当前路由主题色
   int _counter = 0;
 
   void _incrementCounter() {
@@ -69,59 +59,127 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    ThemeData themeData = Theme.of(context);
+    return Theme(
+      data: ThemeData(
+          primarySwatch: _themeColor, //用于导航栏、FloatingActionButton的背景色等
+          iconTheme: IconThemeData(color: _themeColor) //用于Icon颜色
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            new IconButton(
-              icon: new Icon(
-                _counter != 0 ? Icons.favorite: Icons.favorite_border,
-                color: Colors.red,
+      child: Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("计数器"),
+        ),
+                bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          shape: CircularNotchedRectangle(), // 底部导航栏打一个圆形的洞
+          child: Row(
+            children: [
+              // ignore: unnecessary_new
+              new IconButton(
+                icon: Icon(Icons.home),
+                iconSize: 60.0,
+                onPressed: (){
+                  setState(() {
+                    _counter = 0;
+                  });
+                },
               ),
-              iconSize: 100.0,
+              SizedBox(), //中间位置空出
+              // ignore: unnecessary_new
+              new IconButton(
+                icon: Icon(Icons.business),
+                iconSize: 60.0,
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondScreen()));
+                },
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
+          ),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              new IconButton(
+                icon: new Icon(
+                  _counter != 0 ? Icons.favorite: Icons.favorite_border,
+                  color: Colors.red,
+                ),
+                iconSize: 100.0,
+                onPressed: (){
+                  setState(() {
+                    _counter = 0;
+                  });
+                },
+              ),
+              
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+          
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var _themeColor = Colors.yellow; 
+    ThemeData themeData = Theme.of(context);
+    return Theme(
+      data: ThemeData(
+          primarySwatch: _themeColor, //用于导航栏、FloatingActionButton的背景色等
+          iconTheme: IconThemeData(color: _themeColor) //用于Icon颜色
+      ),
+      child: Scaffold(
+        appBar:AppBar(title:Text('动画')),
+        body:Center(child:RaisedButton(
+            child:RaisedButton(
+              child:Text('返回'),
+              color:  Color.fromARGB(255, 248, 237, 17),
               onPressed: (){
-                setState(() {
-                  _counter = 0;
-                });
+                Navigator.pop(context);
               },
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            onPressed: (){
+                Navigator.pop(context);
+              },
+        ))
+     ),
     );
   }
 }
